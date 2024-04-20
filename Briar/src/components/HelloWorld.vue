02877729ca59dd06513446e61c6a3a9a1383b769
Dylan Compton <dylancomptontx@gmail.com>
2024-04-19 21:45:56 -0500

    first commit of the new portfolio! mostly set up

---
+++ Briar/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(false)
+</script>
+
+<template>
+  <h1>dylan briar</h1>
+
+  <div class="card">
+    <button type="button" @click="count = count ? false : true">count is {{ count }}</button>
+    <p>
+      This button will be for light/dark mode eventually
+    </p>
+  </div>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+
+<!-- keeping this here as an example for later
+<style scoped>
+.read-the-docs {
+  color: #cd1313;
+}
+</style> -->
