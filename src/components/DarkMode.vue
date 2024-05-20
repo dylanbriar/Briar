f937de2bcccd66813db564d5909a6cf70ec8bcc5
Dylan Compton <dylancomptontx@gmail.com>
2024-05-20 11:29:08 -0500

    added scss, converted dark mode button to scss

--- src/components/DarkMode.vue
+++ src/components/DarkMode.vue
@@ -24,97 +24,121 @@
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 
 /* provides fading, very subtle */
-div.switcher.switcher-1 input {
-  transition: 0.25s -0.1s;
-} 
-div.switcher.switcher-1 input:checked:before {
-  color: var(--white);
-  transition: color 0.6s -0.2s;
-}
-/* provides a fading background */
-div.switcher.switcher-1 input:not(:checked) {
-  background: var(--black);
-  transition: 0.6s -0.1s;
-}
-/* defines the inner moving shape */
-div.switcher {
+// Define variables
+$switch-width: 150px;
+$switch-height: 40px;
+$switch-radius: 25px;
+$label-radius: 20px;
+
+$white: var(--white);
+$black: var(--black);
+$grey: var(--grey);
+
+// Mixin for shared styles
+@mixin switch-base {
   position: relative;
-  width: 150px;
-  height: 40px;
-  border-radius: 25px;
-}
-div.switcher label {
-  z-index: 1;
-  position: absolute;
-  top: 10px;
-  bottom: 5px;
-  border-radius: 20px;
-}
-/* defines outer shape */
-div.switcher input {
-  appearance: none;
-  position: relative;
-  width: 150px;
-  height: 40px;
-  border-radius: 25px;
-  background-color: var(--black);
-  outline: none;
-  font-family: Garamond, Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-}
-/* defines light/dark text position/color */
-div.switcher input:before,
-div.switcher input:after {
-  z-index: 2;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  color: var(--white);
-}
-/* defines existance of light/dark text respectively  */
-div.switcher input:before {
-  content: 'Light';
-  left: 20px;
-}
-div.switcher input:after {
-  content: 'Dark';
-  right: 20px;
-}
-/* defines outer shape color from default black to white after "checked" (light<--dark) */
-div.switcher.switcher-1 input:checked {
-  transition: 0.6s -0.1s;
-  background-color: var(--white);
-}
-/* defines inner shape color from light<--dark */
-div.switcher.switcher-1 input:checked + label {
-  left: 15px;
-  right: 90px;
-  background: var(--black);
-  transition: left 0.6s, right 0.4s 0.2s;
-}
-/* changes 'Dark' text to grey during light */
-div.switcher.switcher-1 input:checked:after {
-  color: var(--grey);
-  transition: color 0.6s;
-}
-/* changes 'Light' text to grey during dark */
-div.switcher.switcher-1 input:not(:checked):before {
-  color: var(--grey);
-  transition: color 0.6s;
-}
-/* changes 'Dark' text to black during dark */
-div.switcher.switcher-1 input:not(:checked):after {
-  color: var(--black);
-  transition: color 0.6s 0.2s;
-}
-/* defines inner shape color from light-->dark */
-div.switcher.switcher-1 input:not(:checked) + label {
-  left: 100px;
-  right: 10px;
-  background: var(--white);
-  transition: left 0.4s 0.2s, right 0.6s, background 0.35s -0.1s;
+  width: $switch-width;
+  height: $switch-height;
+  border-radius: $switch-radius;
 }
 
+// Styles for the switcher container
+div.switcher {
+  @include switch-base;
+
+  &.switcher-1 {
+    input {
+      transition: 0.25s -0.1s;
+
+      &:checked {
+        &:before {
+          color: $white;
+          transition: color 0.6s -0.2s;
+        }
+
+        // Outer shape color from black to white (light mode)
+        background-color: $white;
+        transition: 0.6s -0.1s;
+
+        // Inner shape color from light to dark
+        + label {
+          left: 15px;
+          right: 90px;
+          background: $black;
+          transition: left 0.6s, right 0.4s 0.2s;
+        }
+
+        // Change 'Dark' text color during light mode
+        &:after {
+          color: $grey;
+          transition: color 0.6s;
+        }
+      }
+
+      &:not(:checked) {
+        // Outer shape color from white to black (dark mode)
+        background: $black;
+        transition: 0.6s -0.1s;
+
+        // Change 'Light' text color during dark mode
+        &:before {
+          color: $grey;
+          transition: color 0.6s;
+        }
+
+        // Change 'Dark' text color during dark mode
+        &:after {
+          color: $black;
+          transition: color 0.6s 0.2s;
+        }
+
+        // Inner shape color from dark to light
+        + label {
+          left: 100px;
+          right: 10px;
+          background: $white;
+          transition: left 0.4s 0.2s, right 0.6s, background 0.35s -0.1s;
+        }
+      }
+    }
+  }
+
+  label {
+    z-index: 1;
+    position: absolute;
+    top: 10px;
+    bottom: 5px;
+    border-radius: $label-radius;
+  }
+
+  input {
+    @include switch-base;
+    appearance: none;
+    background-color: $black;
+    outline: none;
+    font-family: Garamond, Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+
+    &:before,
+    &:after {
+      z-index: 2;
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      color: $white;
+    }
+
+    &:before {
+      content: 'Light';
+      left: 20px;
+    }
+
+    &:after {
+      content: 'Dark';
+      right: 20px;
+    }
+  }
+}
 </style>
