4192fb20d0db3137f83c81cef49eb6635042fdb9
Dylan Compton <dylancomptontx@gmail.com>
2025-02-20 22:57:38 -0600

    feat(App): add Calendly button

--- src/App.vue
+++ src/App.vue
@@ -38,6 +38,10 @@ function handleKeyPress(e: KeyboardEvent): void {
 
 document.addEventListener('keydown', handleKeyPress);
 
+function handleScheduleClick(){
+  window.open("https://calendly.com/dylan-e-briar/30min", "myCalendly")
+}
+
 </script>
 
 <template>
@@ -67,6 +71,7 @@ document.addEventListener('keydown', handleKeyPress);
       <p>&nbsp;&nbsp;&nbsp;&nbsp;To hear about updates to this website, or updates from my engineering journey in general, check out my <a id="socialsTemp" href="https://twitter.com/DylanBriar">profile on X</a> or <a id="socialsTemp" href="https://www.linkedin.com/in/dylanbriar/">LinkedIn.</a></p>
       <p id="comingSoon">Coming Soon: <br> Socials Dropdown switcher <br> Blog posts <br> Fancy Styling <br> (i'm technically fullstack but i'm definitely more comfy on the backend) <br> (don't laugh at me)</p>
   </div>
+  <button class="button-57" role="button" @click="handleScheduleClick()"><span class="text">Want to Chat?</span><span>Schedule a call!</span></button>
 </template>
 
 <style scoped>
@@ -117,6 +122,75 @@ a {
   text-align: center;
   font-size: 16px;
 }
+
+/* credit to Scott Branch on CSS Scan */
+.button-57 {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  overflow: hidden;
+  border: 1px solid #ffffff00;
+  border-radius: 20px;
+  color: #161b16;
+  display: inline-block;
+  font-size: 15px;
+  line-height: 15px;
+  padding: 15px 8px 14px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  text-decoration: none;
+  cursor: pointer;
+  background: #ffffff93;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-57 span:first-child {
+  position: relative;
+  transition: color 600ms cubic-bezier(0.48, 0, 0.12, 1);
+  z-index: 10;
+}
+
+.button-57 span:last-child {
+  color: white;
+  display: block;
+  position: absolute;
+  bottom: 0;
+  transition: all 500ms cubic-bezier(0.48, 0, 0.12, 1);
+  z-index: 100;
+  opacity: 0;
+  top: 50%;
+  left: 50%;
+  transform: translateY(225%) translateX(-50%);
+  height: 14px;
+  line-height: 13px;
+}
+
+.button-57:after {
+  content: "";
+  position: absolute;
+  bottom: -50%;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgb(0, 30, 6);
+  transform-origin: bottom center;
+  transition: transform 600ms cubic-bezier(0.48, 0, 0.12, 1);
+  transform: skewY(9.3deg) scaleY(0);
+  z-index: 50;
+}
+
+.button-57:hover:after {
+  transform-origin: bottom center;
+  transform: skewY(9.3deg) scaleY(2);
+}
+
+.button-57:hover span:last-child {
+  transform: translateX(-50%) translateY(-100%);
+  opacity: 1;
+  transition: all 900ms cubic-bezier(0.48, 0, 0.12, 1);
+}
+/* ^^credit to Scott Branch on CSS Scan^^ */
 
 @media (max-width: 350px) {
   #title-and-switcher {
